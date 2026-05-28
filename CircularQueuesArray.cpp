f954a0379ac8a6db0bb9287ac49f048e03c569c8
suhilmi01 <suhilmi01@mail.com>
2026-05-28 18:23:31 +0700

    loop menu utama dan try-catch

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
 using namespace std;
 
-class Queues {
+class Queues 
+{
 private:
     static const int max = 3;
     int FRONT, REAR;
     int queue_array[max];
 
 public:
-};
-
-int main() {
-    return 0;
-}
-Queues() {
+    Queues() 
+    {
         FRONT = -1;
         REAR = -1;
     }
-    void insert() {
+    void insert() 
+    {
         int num;
         cout << "Enter a number: ";
         cin >> num;
         cout << endl;
-    }
-    if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+    
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) 
+        {
             cout << "\nQueue overflow\n";
             return;
         }
-    if (FRONT == -1) {
+        if (FRONT == -1) 
+        {
             FRONT = 0;
             REAR = 0;
         }
-    else {
+        else 
+        {
             // jika rear berada di posisi terakhir array, kembali ke awal array
             if (REAR == max - 1)
                 REAR = 0;
@@ -39,63 +40,81 @@ Queues() {
                 REAR = REAR + 1;
         }
         queue_array[REAR] = num;
-    void remove() {
+    }
+    void remove() 
+    {
         // cek apakah antrian kosong
-        if (FRONT == -1) {
+        if (FRONT == -1) 
+        {
             cout << "Queue underflow\n";
             return;
         }
-    }
-    cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+    
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
         // cek jika antrian hanya memiliki satu elemen
-        if (FRONT == REAR) {
+        if (FRONT == REAR) 
+        {
             FRONT = -1;
             REAR = -1;
         }
-        else {
+        else 
+        {
             // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
             if (FRONT == max - 1)
                 FRONT = 0;
             else
                 FRONT = FRONT + 1;
         }
-    void display() {
+    }
+    void display()
+    {
         int FRONT_Position = FRONT;
         int REAR_Position = REAR;
 
         // cek apakah antrian kosong
-        if (FRONT_Position == -1) {
+        if (FRONT_Position == -1) 
+        {
             cout << "Queue is empty\n";
             return;
         }
         cout << "\nElements in the queue are....\n";
-    }
-    if (FRONT_Position <= REAR_Position) {
-            while (FRONT_Position <= REAR_Position) {
+
+        if (FRONT_Position <= REAR_Position) 
+        {
+            while (FRONT_Position <= REAR_Position) 
+            {
                 cout << queue_array[FRONT_Position] << " ";
                 FRONT_Position++;
             }
+            cout << endl;
         }
-    else {
+        else
+        {
             // jika FRONT > REAR (Circular Queue berputar)
-            while (FRONT_Position <= max - 1) {
+            while (FRONT_Position <= max - 1) 
+            {
                 cout << queue_array[FRONT_Position] << " ";
                 FRONT_Position++;
             }
             FRONT_Position = 0;
-            while (FRONT_Position <= REAR_Position) {
+            while (FRONT_Position <= REAR_Position) 
+            {
                 cout << queue_array[FRONT_Position] << " ";
                 FRONT_Position++;
             }
         }
-        cout << endl;
-int main() {
+    }
+};
+int main() 
+{
     Queues q;
     char ch;
 
-    while (true) {
-        try {
+    while (true) 
+    {
+        try 
+        {
             cout << "Menu" << endl;
             cout << "1. Implement insert operation" << endl;
             cout << "2. Implement Delete Operation" << endl;
@@ -104,11 +123,34 @@ int main() {
             cout << "Enter your choice (1-4): ";
             cin >> ch;
             cout << endl;
+            
+            switch (ch) {
+            case '1': {
+                q.insert();
+                break;
+            }
+            case '2': {
+                q.remove();
+                break;
+            }
+            case '3': {
+                q.display();
+                break;
+            }
+            case '4': {
+                return 0;
+            }
+            default: 
+            {
+                cout << "Invalid option!!!" << endl;
+                break;
+            }
+            }
         }
-        catch (exception &e) {
+        catch (exception &e) 
+        {
             cout << "check for the values entered." << endl;
         }
     }
     return 0;
 }
-    
