340d959aeea8c71924c66fb66a15515627409948
suhilmi01 <suhilmi01@mail.com>
2026-05-28 18:03:52 +0700

    logika cetak data dan reset saat sisa satu elemen

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -46,3 +46,10 @@ Queues() {
             return;
         }
     }
+    cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
