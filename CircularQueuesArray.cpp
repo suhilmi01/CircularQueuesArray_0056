a20f94235b3a073d007e37b9fbca3ee92cd8010a
suhilmi01 <suhilmi01@mail.com>
2026-05-28 18:02:07 +0700

    menambah validasi antrian penuh

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -23,3 +23,7 @@ Queues() {
         cin >> num;
         cout << endl;
     }
+    if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
